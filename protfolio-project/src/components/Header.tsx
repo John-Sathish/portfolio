38293426a9a76d4c6154976b56affd70d7c68d3b
john sathish <johnsathish2004@gmail.com>
2025-03-28 23:18:30 +0000

    removed unnecessary button

--- protfolio-project/src/components/Header.tsx
+++ protfolio-project/src/components/Header.tsx
@@ -28,7 +28,7 @@ function Header() {
         <img src="/logo.jpg" />
       </div>
 
-      <div className="hidden md:flex gap-14">
+      <div className="hidden md:flex gap-14 justify-center w-full">
         {nav.map((item) => (
           <div
             key={item.id}
@@ -45,7 +45,7 @@ function Header() {
         ))}
       </div>
 
-      <div className="w-[90px] h-[90px] bg-red-500 flex justify-center items-center">
+      {/* <div className="w-[90px] h-[90px] bg-red-500 flex justify-center items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -60,7 +60,7 @@ function Header() {
             d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
           />
         </svg>
-      </div>
+      </div> */}
     </div>
   );
 }
